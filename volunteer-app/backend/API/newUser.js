9c0fc7b0ef9a8ce35bfe5424a77222673ec9a4b9
vaibhav <vblakshmi2021@gmail.com>
2024-07-26 20:35:43 -0400

    adding new user stuff

--- volunteer-app/backend/API/newUser.js
+++ volunteer-app/backend/API/newUser.js
@@ -2,55 +2,60 @@ const User = require('../Schema/User');
 const Organization = require('../Schema/Organization');
 const express = require('express');
 const cors = require('cors');
-
-// Bcrypt for hashing
 const bcryptjs = require('bcryptjs');
 
-// Middleware
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// API call to create new object
 const newUser = async (req, res) => {
-    try {
-      // Using the passed from front-end object
-      const passedUser = req.body;
-      const isOrg = passedUser.isOrg;
-  
-      // Hashing the password
-      var generatedSalt = bcryptjs.genSaltSync(10);
-      var hashedPassword = bcryptjs.hashSync(passedUser.password, generatedSalt);
-  
-      if(!isOrg) {
-        const newUser = new User({
-          username: passedUser.username,
-          password: hashedPassword,
-          first_name: passedUser.firstName,
-          last_name: passedUser.lastName,
-          email: passedUser.email
-        })
+  try {
+    const passedUser = req.body;
+    const isOrg = passedUser.isOrg;
 
-        await newUser.save();
-      }
-      else 
-      {
-        const newOrg = new Organization({
-          username: passedUser.username,
-          password: hashedPassword,
-          organization_name: passedUser.orgName,
-          email: passedUser.email
-        })
+    // Log the incoming request
+    console.log('Received new user registration request:', passedUser);
 
-        await newOrg.save();
-      }
-      // sendEmailVerification({passedEmail: passedUser.email}, res);
-      res.status(200).send('Email sent added successfully');
-  
-  
-    } catch (error) {
-      res.status(500).send('Error adding User: ' + error.message);
+    // Hash the password
+    const generatedSalt = bcryptjs.genSaltSync(10);
+    const hashedPassword = bcryptjs.hashSync(passedUser.password, generatedSalt);
+
+    if (!isOrg) {
+      const newUser = new User({
+        username: passedUser.username,
+        password: hashedPassword,
+        first_name: passedUser.firstName,
+        last_name: passedUser.lastName,
+        email: passedUser.email,
+        userType: 'user'
+      });
+
+      await newUser.save();
+      console.log('New user registered successfully:', newUser);
+    } else {
+      const newOrg = new Organization({
+        username: passedUser.username,
+        password: hashedPassword,
+        organization_name: passedUser.orgName,
+        email: passedUser.email,
+        userType: 'org'
+      });
+
+      await newOrg.save();
+      console.log('New organization registered successfully:', newOrg);
     }
-  };
-  
-  module.exports = newUser;
+
+    res.status(201).send('User/Organization added successfully');
+  } catch (error) {
+    console.error('Error adding user/organization:', error);
+    res.status(500).send('Error adding user/organization: ' + error.message);
+  }
+};
+
+app.post('/new-user', newUser);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+module.exports = newUser;
