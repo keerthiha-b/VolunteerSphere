ccdebb03c81e70188f38ca26673e5ef0332952f9
vaibhav <vblakshmi2021@gmail.com>
2024-08-27 11:21:38 -0400

    added mongoose to the file

--- volunteer-app/backend/API/getProgress.js
+++ volunteer-app/backend/API/getProgress.js
@@ -1,6 +1,7 @@
 const User = require("../Schema/User");
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
